c1e144d749b3d090da1649af67ea5507927a9456
amaani <amaanicabaas280@gmail.com>
2026-06-28 10:28:02 +0300

    aded username

---
+++ test-master/src/test/java/com/test/test/test.java
@@ -0,0 +1,11 @@
+package com.test.test;
+
+public class test {
+    private int id;
+    private String name;
+    private String email;
+    private String passowrd;
+    private String phone;
+    private String telephone;
+
+}
